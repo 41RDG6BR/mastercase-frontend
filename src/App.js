8600d1b11d731803cb575a039eb9915daaf6066c
Rodrigo Nogueira <rdg6design@gmail.com>
2020-04-24 17:59:56 -0300

    frontend filter

--- src/App.js
+++ src/App.js
@@ -5,45 +5,55 @@ import axios from 'axios'
 class App extends Component {
   constructor(props){
     super(props)
-    // this.onChange = this.onChange.bind(this)
+    this.onChange = this.onChange.bind(this)
 
     this.state = {
-      itens: [],
+      itens: {
+        itemsReturned: []
+      },
     }
   }
 
   componentDidMount(){
     axios.get('http://localhost:3000/users').then((result)=>{
       // console.log(result.data)
-      this.setState({ itens: result.data });
+      if(result.data)
+        this.setState({ itens: result.data });
     }).catch((err)=>{
       console.log(err)
     })
   }
 
-  // async componentDidMount() {
-  //   const response = await api.get('');
-  //   // console.log(response.data)
-  //   this.setState({ filmes: response.data });
-  // }
-
-  //   onChange(event){
-  //   const self = this
-  //   const response = api.get('https://api.tvmaze.com/search/shows?q='+event.target.value)
-  //   .then(response=>{
-  //     self.setState({ filmes: response.data });
-  //   })
-  // }
+    onChange(event){
+        axios.get("http://localhost:3000/filter"+event.target.value)
+        .then((result)=>{
+      console.log(result.data)
+      // this.setState({ itens: result.data });
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
 
   render() {
 
-    const itens  = this.state.itens;
-    let result = JSON.stringify(itens)
+    const itens  = this.state.itens
+    let result;
+    if(itens && itens.itemsReturned){
+      result = itens.itemsReturned.map(element => {
+        return (
+          <li key={element.href}>
+            <img src={element.thumbUrl} />
+            <p>{element.name}</p>
+          </li>
+        )
+      });
+    }
  
     return (
       <div>
         <h1>Listar os Filmes</h1>
-    <p>{result}</p>
+           <input onChange={this.onChange} />
+        <ul>{result}</ul>
       </div>
     );
   };
