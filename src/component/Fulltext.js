a5478e23cd34b4a5aef5304a3f6383ca3ed36f57
Rodrigo Nogueira <rdg6design@gmail.com>
2020-04-26 11:50:02 -0300

    Fulltext component

--- src/component/Fulltext.js
+++ src/component/Fulltext.js
@@ -2,8 +2,7 @@ import React from 'react'
 import axios from 'axios'
 
 class FullText extends React.Component{
-
-     constructor(props){
+  constructor(props){
     super(props)
     this.state = {
       fulltext: [],
@@ -20,24 +19,34 @@ class FullText extends React.Component{
     })
   }
 
-    render(){
-        const fulltext = this.state.fulltext
-        let result;
-        // console.log(fulltext)
+  render(){
+      const fulltext = this.state.fulltext        
+      let fulltextSlice = fulltext.slice(0,3)
 
-         if(fulltext){
-            result = fulltext.map(element => {
-        return (
-          <li key={element.productId}>
-          </li>
-        )
-      });
-    }
-        return(<div>
-                <h1>{result}</h1>
-            </div>
-        )
-    }
+      const result = fulltextSlice.map(element => {
+      return (
+        <div className="col-4">
+          <div className="card">               
+            <img className="card-img-top" src={element.items[0].images[0].imageUrl} alt={element.productName} />
+              <div className="card-body">
+                <h4 className="card-title"><a title="Ver produto">{element.productName}</a></h4>
+                  <div className="row">
+                      <div className="col">
+                        <p className="btn btn-danger btn-block">R${element.items[0].sellers[0].commertialOffer.Installments[0].Value}</p>
+                          <a className="btn btn-success btn-block">Link para o produto</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+      )
+    });
+      return(
+        <div className="row">
+          <ul>{result}</ul>
+        </div>
+      )
+    } 
 }
 
 export default FullText
